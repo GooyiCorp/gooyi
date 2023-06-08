07814f48a69848abddd8b5895ca15a6a6850da8d
Thiện Thành Nguyễn <thanhoilathanh482@gmail.com>
2023-06-08 10:38:01 +0200

    switch to django

---
+++ server/gutcheinapp/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+
+class User(models.Model):
+    userid = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    email = models.CharField(max_length=50)
+    password = models.CharField(max_length=20)
+    verified = models.BooleanField(default=False)
