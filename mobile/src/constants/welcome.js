e89e4a39c82604f2cb05e205a220cf46935aeeec
Thiện Thành Nguyễn <thanhoilathanh482@gmail.com>
2023-06-22 03:19:52 +0200

    add fox

--- mobile/src/constants/welcome.js
+++ mobile/src/constants/welcome.js
@@ -1,22 +1,22 @@
 export default welcome = [
     {
         'id': 1,
-        'image': require('../../assets/image/foxSlider1.png'),
+        'image': require('../../assets/image/foxSlide1.png'),
         'text': 'Finde deinen Lieblingsshop und sammle Punkte fur tollee Pramien',
     },
     {
         'id': 2,
-        'image': require('../../assets/image/foxSlider2.png'),
+        'image': require('../../assets/image/foxSlide2.png'),
         'text':'Behalte die besten Deals und Angebote immer im Blick',
     },
     {
         'id': 3,
-        'image': require('../../assets/image/foxSlider2.png'),
+        'image': require('../../assets/image/foxSlide1.png'),
         'text': 'Reservierung einfach und bequem über das Buchungstool vornehmen'
     },
     {
         'id': 4,
-        'image': require('../../assets/image/foxSlider2.png'),
+        'image': require('../../assets/image/foxSlide2.png'),
         'text': 'Willkommen in der Gooyi-app',
     }
 ]
