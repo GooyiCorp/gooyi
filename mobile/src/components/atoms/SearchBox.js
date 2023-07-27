54837acf454a9918766794354766320b571f1de6
ducanhyeuvo <138219976+ducanhyeuvo@users.noreply.github.com>
2023-07-28 01:04:27 +0200

    Update SearchBox.js

--- mobile/src/components/atoms/SearchBox.js
+++ mobile/src/components/atoms/SearchBox.js
@@ -4,7 +4,7 @@ import React from 'react'
 const SearchBox = () => {
   return (
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-        <View style={{width: 300, height: 50, backgroundColor: ''}}>
+        <View style={{width: 300, height: 50, backgroundColor: '#000000'}}>
 
         </View>
     </View>
