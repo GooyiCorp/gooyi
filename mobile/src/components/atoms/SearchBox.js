581a1285f9abfd4db40a0191cb06d17a6d80db86
ducanhyeuvo <138219976+ducanhyeuvo@users.noreply.github.com>
2023-07-28 02:07:13 +0200

    eror

--- mobile/src/components/atoms/SearchBox.js
+++ mobile/src/components/atoms/SearchBox.js
@@ -1,13 +1,32 @@
-import { View, Text } from 'react-native'
+import { View, Text, TextInput, TouchableOpacity } from 'react-native'
 import React from 'react'
+import { Feather } from '@expo/vector-icons'; 
+import Animated, { useAnimatedStyle, useSharedValue, withTiming } from 'react-native-reanimated';
+
+
 
 const SearchBox = () => {
+  const animation = useSharedValue(0);
+  const animatedStyle = useAnimatedStyle(() => {
+    return {
+      width: animation.value==1?withTiming(300,{duration:500}):withTiming(0,{duration:500}),
+    }
+  })
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-        <View style={{width: 300, height: 50, backgroundColor: '#000000'}}>
-
+    <Animated.View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <View style={[{width: 300, height: 50, backgroundColor: '#dfdede', borderRadius: 10, flexDirection: 'row', alignItems: 'center'}, animatedStyle,]}>
+          <TextInput style={{width: '85%'}} placeholder={'Search something...'}/>
+          <TouchableOpacity onPress={() => {
+            if (animation.value==1) {
+              animation.value = 0;
+            } else {
+              animation.value = 1;
+            }
+          }}>
+            <Feather name="search" size={24} color="black" />
+          </TouchableOpacity>
         </View>
-    </View>
+    </Animated.View>
   )
 }
 
--- mobile/src/screens/main-screens/DiscoverScreen.js
+++ mobile/src/screens/main-screens/DiscoverScreen.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { StyleSheet,View, Text } from 'react-native'
 import { Avatar } from 'react-native-paper';
+import SearchBox from '../../components/atoms/SearchBox';
 
 
 export default function DiscoverScreen() {
@@ -8,6 +9,7 @@ export default function DiscoverScreen() {
     <View style={styles.screen}>
       <Text>Entdecke</Text>
       <Avatar.Text size={24} label="XD" />
+      <SearchBox />
     </View>
   )
 }
