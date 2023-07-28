3a7cbc5f8d7642b639d6cd2ceefce0781b9721ec
ducanhyeuvo <138219976+ducanhyeuvo@users.noreply.github.com>
2023-07-28 09:52:44 +0200

    n

--- mobile/src/components/atoms/SearchBox.js
+++ mobile/src/components/atoms/SearchBox.js
@@ -1,6 +1,6 @@
-import { View, Text, TextInput, TouchableOpacity } from 'react-native'
+import { View, Text, TextInput, TouchableOpacity, StyleSheet} from 'react-native'
 import React from 'react'
-import { Feather } from '@expo/vector-icons'; 
+import { Ionicons } from '@expo/vector-icons'; 
 import Animated, { useAnimatedStyle, useSharedValue, withTiming } from 'react-native-reanimated';
 
 
@@ -9,28 +9,53 @@ const SearchBox = () => {
   const animation = useSharedValue(0);
   const animatedStyle = useAnimatedStyle(() => {
     return {
-      width: animation.value==1?withTiming(300,{duration:500}):withTiming(0,{duration:500}),
+      width: animation.value==1?withTiming(200,{duration:500}):withTiming(0,{duration:500}),
+      opacity: animation.value==1?withTiming(1,{duration:200}):withTiming(0,{duration:200}),
     }
   })
   return (
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'flex-start', width: '80%'}}>
       <View style={{ flexDirection: 'row', alignItems: "center"}}>
-        <Animated.View style={[{width: 300, height: 50, backgroundColor: '#dfdede', borderRadius: 10, justifyContent: 'center'}, animatedStyle]}>
-          <TextInput style={{width: '85%'}} placeholder={'Search something...'}/>
-        </Animated.View>
-          <TouchableOpacity onPress={() => {
+          <TouchableOpacity 
+          style={styles.icon}
+          onPress={() => {
             if (animation.value==1) {
               animation.value = 0;
             } else {
               animation.value = 1;
             }
-          }}
+          }} 
           >
-            <Feather name="search" size={24} color="black" />
+            <Ionicons name={animation.value==1?"close":"search"} size={20} color="black" />
           </TouchableOpacity>
+        <Animated.View style={[styles.searchContainer, animatedStyle]}>
+          <TextInput style={{width: '85%'}} placeholder={'Search something...'}/>
+        </Animated.View>
       </View>
     </View>
   )
 }
 
 export default SearchBox
+
+const styles = StyleSheet.create ({
+  icon: {
+    width: 30,
+    height: 30,
+    backgroundColor: '#eeeeee',
+    borderRadius: '50%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginRight: 10
+  },
+
+  searchContainer: {
+    width: 200,
+    height: 40,
+    backgroundColor: '#eeeeee',
+    borderRadius: '50%',
+    justifyContent: 'center',
+    padding: 10,
+
+  }
+})
