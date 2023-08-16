193a335a13b7b88e38c52063f68ba7ac2cd504ed
ducanhyeuvo <138219976+ducanhyeuvo@users.noreply.github.com>
2023-08-16 04:24:47 +0200

    goback

--- mobile/src/screens/main-screens/QRCodeScreen.js
+++ mobile/src/screens/main-screens/QRCodeScreen.js
@@ -2,13 +2,13 @@ import { StyleSheet, Text, View, Button } from 'react-native'
 import React from 'react'
 import { BlurView } from 'expo-blur'
 
-export default function QRCodeScreen({navigation}) {
+export default function QRCodeScreen({navigation: {goBack}}) {
   return (
     <View style={{flex: 1}}>
         <BlurView intensity={16}  style={{flex:1, alignItems: 'center', justifyContent: 'center', backgroundColor: 'rgba(0, 0, 0, 0.5)' }}>
         <View style={{width:363, height:500, backgroundColor: 'white', alignItems: 'center', justifyContent: 'center', borderRadius: 16}}>
             <Text>QRCodeScreen</Text>
-            <Button title='back' onPress={() => navigation.navigate('Home')}/>
+            <Button title='back' onPress={() => goBack()}/>
         </View>
         </BlurView>
     </View>
