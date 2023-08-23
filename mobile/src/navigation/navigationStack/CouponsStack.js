c2034ff8d5f758e7230cdfe1458773b2f90cbf4d
ducanhyeuvo <138219976+ducanhyeuvo@users.noreply.github.com>
2023-08-24 00:04:42 +0200

    fix

--- mobile/src/navigation/navigationStack/CouponsStack.js
+++ mobile/src/navigation/navigationStack/CouponsStack.js
@@ -8,6 +8,7 @@ import { ROUTES } from '../../index/constantsindex';
 import { View } from 'react-native';
 import Animated, { interpolate, useSharedValue, withTiming, useAnimatedStyle, useAnimatedRef } from 'react-native-reanimated';
 import { Button } from 'react-native-paper';
+import { useIsFocused } from '@react-navigation/native';
 
 //---------------------------------------------------------------------------------------------------------------------
 
@@ -16,14 +17,16 @@ const CouponsStack = createStackNavigator();
 //---------------------------------------------------------------------------------------------------------------------
 
 export default function CouponsStackNav({route, navigation}) {
-  const {testValue} = route.params;
+  const isFocused = useIsFocused()
+  const scaleValue = useSharedValue(0)
+  //const {testValue} = route.params;
   //console.log(testValue)
-  const newVal = testValue
-  const scaleValue = useSharedValue(newVal)
-  scaleValue.value = withTiming(1,{duration: 1000}, (finished) => {
+
+
+  scaleValue.value = withTiming(1,{duration: 3000}, (finished) => {
     if (finished) {
       console.log('ANIMATION ENDED');
-      //scaleValue.value = 0
+      isFocused? scaleValue.value = 1 : scaleValue.value = 0
     } else {
       console.log('ANIMATION GOT CANCELLED');
     }
@@ -37,8 +40,7 @@ export default function CouponsStackNav({route, navigation}) {
           }
         }
   )
-  console.log(newVal);
-  console.log(scaleValue.value);
+  console.log(scaleValue.value)
 
   return (
     <>
