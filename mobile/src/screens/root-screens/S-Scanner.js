f82497cb03bc0fb8a51f1aa5e8aa6e16cf5e1850
ducanhyeuvo <138219976+ducanhyeuvo@users.noreply.github.com>
2023-09-20 04:04:49 +0200

    Testing return scan Data

--- mobile/src/screens/root-screens/S-Scanner.js
+++ mobile/src/screens/root-screens/S-Scanner.js
@@ -11,14 +11,16 @@ export default function Scanner() {
 
   const [hasPermission, setHasPermission] = useState(null);
   const [scanned, setScanned] = useState(false);
-
-  const boundValue = useSharedValue(0)
-
-
+  const [dataTest, setDataTest] = useState(false);
+  
   const [boundX, setBoundX] = useState(0)
   const [boundY, setBoundY] = useState(0)
   const [sizeWidth, setSizeWidth] = useState(0)
   const [sizeHeight, setSizeHeight] = useState(0)
+
+  const [dataReturn, setDataReturn] = useState()
+  
+  const boundValue = useSharedValue(0)
 
   const showBound = useAnimatedStyle(() =>{
     const scale = interpolate(boundValue.value, [0, 1, 2], [1.5, 1, 1])
@@ -41,15 +43,22 @@ export default function Scanner() {
     getBarCodeScannerPermissions();
   }, []);
 
-  const handleBarCodeScanned = (context, type, data) => {
+  const handleBarCodeScanned = (context, type) => {
     //setScanned(true);
+    setDataTest(true)
     const {origin, size} = context.bounds
+    const data = context.data
     boundValue.value = withSequence(withTiming(1, {duration: 500, easing: Easing.bezier(0.03, 0.68, 0.5, 1.13)}), withDelay(300, withTiming(2, {duration: 300})), withTiming(0, {duration: 0}) )
     setBoundX(origin.x)
     setBoundY(origin.y)
     setSizeHeight(size.height)
     setSizeWidth(size.width)
     
+    
+    //console.log(data)
+
+    setDataReturn(data)
+
     //alert(`Bar code with type ${type} and data ${data} has been scanned!`);
     //console.log(context)
   };
@@ -85,10 +94,16 @@ return (
         <BoundBox showBoundingBox boxHeight={sizeHeight} boxWidth={sizeWidth} top={boundY} left={boundX}/>
         </Animated.View>
 
+        {/* Data Box */}
+        {dataTest && <TouchableOpacity style={{backgroundColor: '#ffffff', borderRadius: 50, opacity: 0.5, position: 'absolute', justifyContent: 'center', alignItems: 'center', margin: 10}} onPress={() => setDataTest(false)}>
+          <Text style={{margin: 10}}>{dataReturn}</Text>
+        </TouchableOpacity>}
+
         </BarCodeScanner>
         {scanned && <Button title={'Tap to Scan Again'} onPress={() => (setScanned(false), boundValue.value=0)} />}
     
       </View>
+
 
       {/* Direction Container -------------------------------------------- */}
       <View style={styles.directionContainer}>
