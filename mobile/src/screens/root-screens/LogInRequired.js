eac03a32c5767dcf64772c7fd33f80b2f45a02e4
ducanhyeuvo <138219976+ducanhyeuvo@users.noreply.github.com>
2023-11-08 05:39:06 +0100

    logInRequired

---
+++ mobile/src/screens/root-screens/LogInRequired.js
@@ -0,0 +1,53 @@
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import React from 'react'
+import { height, width } from '../../constants/size'
+import { COLORS } from '../../index/constantsindex'
+import { moderateScale } from '../../helper/scale'
+import BigButton from '../../components/components_LogIn/BigButton'
+import { useNavigation } from '@react-navigation/native'
+import { H1, H3, T1, T2, T4 } from '../../constants/text-style'
+
+export default function LogInRequired({}) {
+    const navigation = useNavigation()
+  return (
+    <View style={styles.mainCard}>
+
+        <View style={{width: width-60, height: width-60, justifyContent: 'center', alignItems: 'center'}}>
+            <Image source={require('../../../assets/image/foxLock.png')} resizeMode='contain' style={{maxWidth: '95%'}}/>
+        </View>
+               
+        <Text style={[H1, {fontSize: 50, marginTop: -15, textAlign: 'center'}]}>Oooops!</Text>
+        <Text style={[H3, {fontFamily:'RH-Medium',textAlign: 'center', marginTop: 10}]}>Anmeldung erforderlich.</Text>
+        
+        <Text style={[T1, {marginTop: 10, textAlign:'center', marginBottom: 20}]}>Um diese Seite aufrufen zu können, {"\n"}musst du angemeldet sein.</Text>
+
+        <BigButton 
+            title={'Mit E-Mail Anmelden'} 
+            bgStyle={{backgroundColor: COLORS.primary, maxWidth: '100%', borderRadius: 16}}
+            titleStyle={{color: COLORS.white, fontFamily: 'RH-Medium'}}
+            onPress={() => navigation.navigate('RegisterEmail')}
+        />
+        <BigButton 
+            title={'Mit Telefonnumer Anmelden'}
+            bgStyle={{backgroundColor: '#e3ded8', maxWidth: '100%', borderRadius: 16}}
+            titleStyle={{color: COLORS.black, fontFamily: 'RH-Medium'}}
+        />
+
+        <Text style={[T4, {marginBottom: 80, marginTop: 20, textAlign: 'center'}]}>Über die Speicherung und Verarbeitung personenbezogener Daten findest du in unsere <Text onPress={() => console.log('press Datenschutz')} style={[T4, {fontFamily: 'RH-Bold', color: COLORS.primary}]}>Datenschutzerklärung</Text> alle wichtige Erläuterungen. Bitte liest Sie sorgfältig durch!</Text>
+        
+    </View>
+
+  )
+}
+
+const styles = StyleSheet.create({
+
+    mainCard: {
+        width: width,
+        height: height,
+        backgroundColor: COLORS.mainBackground,
+        padding: 30,
+        justifyContent: 'flex-end',
+    },
+    
+})
--- mobile/src/screens/root-screens/S-Coupons.js
+++ mobile/src/screens/root-screens/S-Coupons.js
@@ -7,6 +7,8 @@ import SortByShop from '../../components/molecules/SortByShop'
 import { useNavigation } from '@react-navigation/native'
 import { height, width } from '../../constants/size'
 import SearchModal from '../../components/components_universal/SearchModal'
+import LogInRequired from './LogInRequired'
+import { useSelector } from 'react-redux'
 
 
 
@@ -19,6 +21,8 @@ export default function CouponsScreen({
 }) {
 
   const navigation = useNavigation()
+
+  const logIn = !useSelector((state) => state.user.isLoggedIn)
 
   // Search Modal ----------------------------------------------------------------------
   const [showSearchModal, setShowSearchModal] = useState(false)
@@ -34,6 +38,8 @@ export default function CouponsScreen({
 
   return (
     <View style={{height: height, width: width}}>
+
+      {logIn && <LogInRequired />}
 
       {showSearchModal && <SearchModal onClose={onCloseSearchModal}/>}
 
