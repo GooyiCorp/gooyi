0e0e3c87794d5a958b1833f5c7b7ea96428bd3ee
Thanh Nguyen <thanhoilathanh482@gmail.com>
2023-11-25 07:31:48 +0100

    delete django

--- server/gutcheinapp/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.contrib.gis import admin as geoAdmin
-from .models import *
-# Register your models here.
-
-admin.site.register(User)
-admin.site.register(UserVersuche)
-geoAdmin.site.register(Store, geoAdmin.OSMGeoAdmin)
--- server/gutcheinapp/utils/opt.py
+++
@@ -1,4 +0,0 @@
-import random
-
-def OTP_generator():
-    return str(random.randint(100000, 999999))
