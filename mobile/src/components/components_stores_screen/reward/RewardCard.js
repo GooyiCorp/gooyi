326ea895e675db1854e9800247ebf98424aecc2c
ducanhyeuvo <138219976+ducanhyeuvo@users.noreply.github.com>
2023-12-17 07:15:30 +0100

    Reward Page

--- mobile/src/components/components_stores_screen/reward/RewardCard.js
+++ mobile/src/components/components_stores_screen/reward/RewardCard.js
@@ -4,32 +4,72 @@ import { width } from '../../../constants/size'
 import { COLORS } from '../../../index/constantsindex'
 import Icons, { icons } from '../../components_universal/Icons'
 import PointIcon from '../../components_universal/PointIcon'
-import { H3, H4, T1 } from '../../../constants/text-style'
+import { H3, H4, T1, T2, T3, T4 } from '../../../constants/text-style'
 import RoundButton from '../../components_universal/RoundButton'
+import IconLabelButton from '../../components_universal/IconLabelButton'
 
 export default function RewardCard() {
   return (
     <View style={styles.card}>
-        {/* <View style={{height: 40, width: 40, backgroundColor: COLORS.ivoryDark2, justifyContent: 'center', alignItems: 'center', borderRadius: 10}}>
-            <Icons
-                icon={icons.MaterialCommunityIcons}
-                iconName={'ticket-percent'}
-                iconSize={28}
-                iconColor={COLORS.ivoryDark}
-            />
-        </View> */}
-      <Text>Kostenloses Getränk Size M</Text>
 
-      <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
-      <View style={{flexDirection: 'row', marginTop: 10}}>
-      <PointIcon style={{marginRight: 5}}/>
-      <Text style={[H3, {fontFamily: 'RH-Bold', color: COLORS.grey}]}>200</Text>
-      </View>
-      <View style={{backgroundColor: COLORS.primary, alignSelf: 'flex-end', paddingHorizontal: 10, paddingVertical: 5, borderRadius: 10}}>
+        <Icons
+            icon={icons.Ionicons}
+            iconName={'ios-information-circle-outline'}
+            iconSize={20}
+            iconColor={COLORS.grey}
+            iconStyle={{
+                position: 'absolute',
+                top: 5,
+                right: 5, 
+            }}
+        />
 
-        <Text style={[T1, {color: COLORS.white}]}>Einlösen</Text>
-      </View>
-      </View>
+
+        {/* <Text style={T4}>Dat Backhus</Text> */}
+        
+        <View style={{flexDirection: 'row', margin: 5}}>
+
+        <Icons
+            icon={icons.MaterialCommunityIcons}
+            iconName={'ticket-percent'}
+            iconSize={20}
+            iconColor={COLORS.grey}
+        />
+
+        <View style={{marginLeft: 10}}>
+            <Text style={[T1, {fontFamily: 'RH-Bold', color: COLORS.grey}]}>1x kostenloses Getränk</Text>
+            <Text style={[T2, {fontFamily: 'RH-Medium', color: COLORS.grey}]}>Größe M</Text>
+        </View>
+
+        </View>
+
+        {/* <View style={{width: '100%', borderWidth: 0.5, borderColor: COLORS.ivoryDark2}}></View> */}
+        <View style={{flexDirection: 'row', justifyContent: 'flex-end', alignItems: 'center'}}>
+
+
+            <View style={{flexDirection: 'row', alignItems: 'center', marginRight: 15}}>
+                <Text style={[H3, {fontFamily: 'RH-Bold', color: COLORS.grey}]}>200</Text>
+                <PointIcon style={{marginLeft: 5, backgroundColor: COLORS.primary}}/>
+            </View>
+        
+
+
+            {/* Button */}
+            <View>
+                <IconLabelButton 
+                label={'Einlösen'}
+                style={{
+                    backgroundColor: COLORS.ivoryDark,
+                    paddingHorizontal: 15,
+                }}
+                labelStyle={{
+                    fontFamily: 'RH-Medium',
+                    color: COLORS.grey
+                }}
+                />
+            </View>
+            
+        </View>
     </View>
   )
 }
@@ -37,9 +77,13 @@ export default function RewardCard() {
 const styles = StyleSheet.create({
     card: {
         width: width-60,
-        marginHorizontal: 30,
         backgroundColor: COLORS.ivory,
         borderRadius: 16,
         padding: 10,
+    },
+
+    text: {
+        color: COLORS.grey,
+        fontFamily: 'RH-Medium',
     }
 })
--- mobile/src/screens/sub-screens/store_screens/AwardOverview.js
+++ mobile/src/screens/sub-screens/store_screens/AwardOverview.js
@@ -1,25 +1,60 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Image, StyleSheet, Text, View } from 'react-native'
+import React, { useState } from 'react'
 import SettingHeader from '../../../navigation/navigationComponents/SettingHeader'
 import { COLORS } from '../../../index/constantsindex'
 import { height, width } from '../../../constants/size'
 import RewardCard from '../../../components/components_stores_screen/reward/RewardCard'
+import { T3 } from '../../../constants/text-style'
+import PointIcon from '../../../components/components_universal/PointIcon'
+import AnimatedText from '../../../components/components_universal/pointCounter/AnimatedText'
+import { useSelector } from 'react-redux'
 
 export default function AwardOverview({
   navigation: {goBack}
 }) {
+  const [headerHeight, setHeaderHeight] = useState(0)
+  const point = useSelector((state) => state.point.point)
+  const handleGoBack = () => {
+    goBack()
+  }
   return (
     <View style={styles.card}>
-      {/* Header */}
-      <SettingHeader
-            goBack
-            onPressGoBack={() => goBack()}
-            header
-            headerText={'Prämienübersicht'}
-            iconStyle={COLORS.mainBackground}
-        />
-      
+      {/* ------------------------------------------------------------- */}
+      {/* Header Section */}
+      {/* ------------------------------------------------------------- */}
+      <View style={{paddingBottom: 30}} onLayout={(e) => setHeaderHeight(e.nativeEvent.layout.height)}>
+        {/* Header Back Button */}
+        <SettingHeader
+          goBack
+          onPressGoBack={handleGoBack}
+          header
+          headerText={'Prämienübersicht'}
+          iconStyle={COLORS.mainBackground}
+        />    
+        {/* Information Section */}
+        <View style={{paddingHorizontal: 30, flexDirection: 'row', justifyContent: 'space-between', alignItems: 'flex-end'}}>
+          <View>
+            {/* Label */}
+            <Text style={[T3, {color: COLORS.grey}]}>Meine Punkte</Text>
+            {/* Points */}
+            <View style={{flexDirection: 'row'}}>
+                {/* <Text style={[H2, {color: COLORS.grey, marginRight: 5}]}>500</Text> */}
+                <PointIcon style={{marginTop: 18, marginRight: 5}}/>
+                <AnimatedText num={point} duration={1000}/>
+            </View>
+          </View>
+          {/* Logo Box */}
+          <View style={styles.logoBox}>
+            <Image source={require('../../../../assets/image/datbackhus.png')} resizeMode='contain' style={{maxWidth: '80%'}}/>
+          </View>
+        </View>
+      </View>
+      {/* ------------------------------------------------------------- */}
+      {/* Content Section */}
+      {/* ------------------------------------------------------------- */}
+      <View style={[styles.contentSection, {height: height-headerHeight}]}>
       <RewardCard />
+      </View>
     </View>
   )
 }
@@ -30,4 +65,22 @@ const styles = StyleSheet.create({
     width: width,
     backgroundColor: COLORS.mainBackground,
   },
+
+  logoBox: {
+    height: 60,
+    width: 60,
+    backgroundColor: COLORS.white,
+    borderRadius: 10,
+    justifyContent: 'center', 
+    alignItems: 'center',
+    overflow: 'hidden'
+  },
+
+  contentSection: {
+    width: width,
+    height: height,
+    backgroundColor: COLORS.white,
+    paddingHorizontal: 30,
+    paddingVertical: 30
+  }
 })
