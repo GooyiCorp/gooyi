7dba37cd0e30de063557b69e04821b289957970f
ducanhyeuvo <138219976+ducanhyeuvo@users.noreply.github.com>
2023-12-19 09:30:03 +0100

    sdfgsdfg

--- mobile/src/screens/detail-screens/OfferCardDetail.js
+++ mobile/src/screens/detail-screens/OfferCardDetail.js
@@ -2,10 +2,19 @@ import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { height, width } from '../../constants/size'
 import { COLORS } from '../../index/constantsindex'
+import SettingHeader from '../../components/components_navigation/SettingHeader'
 
 export default function OfferCardDetail() {
   return (
     <View style={styles.card}>
+      {/* Header Back Button */}
+      <SettingHeader
+        goBack
+        // onPressGoBack={handleGoBack}
+        header
+        headerText={'Vorschau'}
+        iconStyle={COLORS.mainBackground}
+      />   
         <View style={styles.imageBox}></View>
     </View>
   )
@@ -15,14 +24,13 @@ const styles = StyleSheet.create({
     card: {
         height: height,
         width: width,
-        padding: 30,
         backgroundColor: COLORS.mainBackground
     },
     imageBox: {
-        marginTop: 20,
         height: 0.25*height,
-        width: width-60,
-        backgroundColor: COLORS.ivory,
+        width: width-40,
+        backgroundColor: COLORS.ivoryDark,
         borderRadius: 16,
+        marginHorizontal: 20
     }
 })
