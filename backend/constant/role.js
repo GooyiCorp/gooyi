0cf28cb43fc6b59ee31eb8889ed94fe0e17152e2
Thanh Nguyen <thanhoilathanh482@gmail.com>
2024-03-21 11:11:04 +0100

    Update role.js

--- backend/constant/role.js
+++ backend/constant/role.js
@@ -1,3 +1,10 @@
 export const USER = "user";
 export const STORE = "store";
 export const ADMIN = "admin";
+
+
+export const STORE_ROLE = {
+    ADMIN: "store_admin",
+    MODERATOR: "store_moderator",
+    MEMBER: "store_member",
+}
